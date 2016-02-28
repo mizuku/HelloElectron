de49152846811b7f67edb0ab5dc77cb008b52dff
Mizuku <mizuku.ga@gmail.com>
2016-02-28 21:01:22 +0900

    electron --debug ./ bad project.

---
+++ HelloElectron/Scripts/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="knockout/knockout.d.ts" />
+/// <reference path="knockout/tests/jasmine.extensions.d.ts" />
+/// <reference path="github-electron/electron-prebuilt.d.ts" />
+/// <reference path="github-electron/github-electron.d.ts" />
+/// <reference path="node/node.d.ts" />
